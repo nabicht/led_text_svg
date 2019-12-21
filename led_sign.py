54972d7b1a832047731935e66f60d9819f0538d8
Peter F. Nabicht <nabicht@gmail.com>
2019-12-21 07:16:17 -0600

    added ablity to dump settings to json

--- led_sign.py
+++ led_sign.py
@@ -1,11 +1,41 @@
+"""
+This file is part of led_text_svg.
 
+MIT License
+
+Copyright (c) 2016-2017 Peter F. Nabicht
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+
+import json
 import svgwrite
 
 class LEDSign(object):
 
     def __init__(self, font, kerning=2, space=5, point_radius=4, point_space=2):
         assert kerning > 0, "kerning must be greater than 0"
-        assert isinstance(point_radius, int), "point radius must be an int"
+        assert space >= 0, "space must be greater than or equal to 0"
+        assert font is not None, "font must be defined"
+        assert point_radius > 0, "point_radius must be greater than 0"
+        assert point_space >= 0, "point space must be greater than or equal to 0"
         self._font = font
         self._kerning = kerning
         self._space =  space
@@ -46,5 +76,12 @@ class LEDSign(object):
             starting_point = self._generate_character(starting_point, dwg, char)
         dwg.save()
 
+    def settings(self):
+        d = {"font": self._font.__class__.__name__,
+             "kerning": self._kerning,
+             "space": self._space,
+             "point_radius": point_radius,
+             "point_space": point_space}
+        return json.dumps(d)
 
 
